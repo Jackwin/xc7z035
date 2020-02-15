bdc2bc95cd5ac4c46c1fd6586c337e0eef2d58b5
Jackwin <rcwcj@126.com>
2020-02-15 20:18:13 +0800

    ADC DDR @ 250MHz timing Passed

--- source/constr/adc_timing.xdc
+++ source/constr/adc_timing.xdc
@@ -1,14 +1,11 @@
-create_clock -period 3.333 -name adc0_clk -waveform {0.000 2.000} [get_ports adc0_dco_p]
+create_clock -period 4.00 -name adc0_clk -waveform {0.000 2.000} [get_ports adc0_dco_p]
 
 #set_input_delay -clock [get_clocks adc0_clk] -min -0.223 [get_ports -filter { NAME =~  "*adc0_din_p*" && DIRECTION == "IN" }]
 #set_input_delay -clock [get_clocks adc0_clk] -max -0.223 [get_ports -filter { NAME =~  "*adc0_din_p*" && DIRECTION == "IN" }]
 #set_input_delay -clock [get_clocks adc0_clk] -clock_fall -fall -min -add_delay -0.223 [get_ports -filter { NAME =~  "*adc0_din_p*" && DIRECTION == "IN" }]
 #set_input_delay -clock [get_clocks adc0_clk] -clock_fall -fall -max -add_delay -0.223 [get_ports -filter { NAME =~  "*adc0_din_p*" && DIRECTION == "IN" }]
 
-set_input_delay -clock [get_clocks adc0_clk] -min -0.07 [get_ports -filter { NAME =~  "*adc0_din_p*" && DIRECTION == "IN" }]
-set_input_delay -clock [get_clocks adc0_clk] -max -0.07 [get_ports -filter { NAME =~  "*adc0_din_p*" && DIRECTION == "IN" }]
-set_input_delay -clock [get_clocks adc0_clk] -clock_fall -fall -min -add_delay -0.07 [get_ports -filter { NAME =~  "*adc0_din_p*" && DIRECTION == "IN" }]
-set_input_delay -clock [get_clocks adc0_clk] -clock_fall -fall -max -add_delay -0.07 [get_ports -filter { NAME =~  "*adc0_din_p*" && DIRECTION == "IN" }]
+
 
 #set_property LOC IDELAY_X0Y210 [get_cells ad9434_data_i/idelay[0].IDELAYE2_dco]
 #set_property LOC IDELAY_X0Y216 [get_cells ad9434_data_i/idelay[1].IDELAYE2_dco]
@@ -18,12 +15,84 @@ set_input_delay -clock [get_clocks adc0_clk] -clock_fall -fall -max -add_delay -
 #set_property LOC IDELAY_X0Y204 [get_cells ad9434_data_i/idelay[5].IDELAYE2_dco]
 
 set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/IDELAYCTRL_dco]
-set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[0].IDELAYE2_dco]
-set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[1].IDELAYE2_dco]
-set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[2].IDELAYE2_dco]
-set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[3].IDELAYE2_dco]
-set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[4].IDELAYE2_dco]
-set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[5].IDELAYE2_dco]
+set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[0].IDELAYE2_data]
+set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[1].IDELAYE2_data]
+set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[2].IDELAYE2_data]
+set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[3].IDELAYE2_data]
+set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[4].IDELAYE2_data]
+set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay[5].IDELAYE2_data]
+
+
+set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/idelay_mode.IDELAYE2_clk]
+#set_property IODELAY_GROUP  adc_idelay_grp [get_cells ad9434_data_i/IDELAYE2_clk]
+# Edge-Aligned Double Data Rate Source Synchronous Inputs 
+# (Using an MMCM/PLL)
+#
+# For an edge-aligned Source Synchronous interface, the clock
+# transition occurs at the same time as the data transitions.
+# In this template, the clock is aligned with the end of the
+# data. The constraints below rely on the default timing
+# analysis (setup = 1/2 cycle, hold = 0 cycle).
+#
+# input                        ___________________________
+# clock  _____________________|                           |__________
+#                             |                           |                 
+#                     skew_bre|skew_are           skew_bfe|skew_afe
+#                     <------>|<------>           <------>|<------>
+#          ___________        |        ___________                 __
+# data   XX_Rise_Data_XXXXXXXXXXXXXXXXX_Fall_Data_XXXXXXXXXXXXXXXXX
+#
+
+
+
+#create_clock -period 4 -name virt_clk -waveform {0.00 2.00}
+#create_generated_clock -name adc0_clk_90 -source [get_pins ad9434_data_i/mmcm_mode.mmcm_adv_inst/CLKIN1] -edges {1 2 3} \ -edge_shift {1 1 1} [get_pins ad9434_data_i/mmcm_mode.mmcm_adv_inst/CLKOUT0]
+
+#set_false_path -setup -rise_from [get_clocks virt_clk] -fall_to [get_clocks -of_objects [get_pins ad9434_data_i/mmcm_mode.clk_wiz_inst/inst/mmcm_adv_inst/CLKOUT0]]
+#set_false_path -setup -fall_from [get_clocks virt_clk] -rise_to [get_clocks -of_objects [get_pins ad9434_data_i/mmcm_mode.clk_wiz_inst/inst/mmcm_adv_inst/CLKOUT0]]
+
+#set_false_path -hold -rise_from [get_clocks virt_clk] -rise_to [get_clocks -of_objects [get_pins ad9434_data_i/mmcm_mode.clk_wiz_inst/inst/mmcm_adv_inst/CLKOUT0]]
+#set_false_path -hold -fall_from [get_clocks virt_clk] -fall_to [get_clocks -of_objects [get_pins ad9434_data_i/mmcm_mode.clk_wiz_inst/inst/mmcm_adv_inst/CLKOUT0]]
+
+
+create_clock -period 4 -name virt_clk -waveform {0.00 2.00}
+set_multicycle_path 0 -from [get_clocks virt_clk] -to [get_clocks adc0_clk]
+set_false_path -setup -rise_from [get_clocks virt_clk] -fall_to [get_clocks adc0_clk]
+set_false_path -setup -fall_from [get_clocks virt_clk] -rise_to [get_clocks adc0_clk]
+
+set_multicycle_path -1 -hold -from [get_clocks virt_clk] -to [get_clocks adc0_clk]
+set_false_path -hold -rise_from [get_clocks virt_clk] -rise_to [get_clocks adc0_clk]
+set_false_path -hold -fall_from [get_clocks virt_clk] -fall_to [get_clocks adc0_clk]
+
+
+
+#set input_ports         adc0_din_n[*];     # List of input ports
+
+#set_input_delay -clock virt_clk 2.07 [get_ports $input_ports]
+#set_input_delay -clock virt_clk 1.93 -min [get_ports $input_ports]
+#set_input_delay -clock virt_clk 2.07 [get_ports $input_ports] -clock_fall -add_delay
+#set_input_delay -clock virt_clk 1.93 -min [get_ports $input_ports] -clock_fall -add_delay
+
+set input_clock         virt_clk;      # Name of input clock
+set skew_bre            0.07;             # Data invalid before the rising clock edge
+set skew_are            0.07;             # Data invalid after the rising clock edge
+set skew_bfe            0.07;             # Data invalid before the falling clock edge
+set skew_afe            0.07;             # Data invalid after the falling clock edge
+set input_ports         adc0_din_n[*];     # List of input ports
+
+# Input Delay Constraint
+set_input_delay -clock $input_clock -max $skew_are  [get_ports $input_ports];
+set_input_delay -clock $input_clock -min -$skew_bre [get_ports $input_ports];
+set_input_delay -clock $input_clock -max $skew_afe  [get_ports $input_ports] -clock_fall -add_delay;
+set_input_delay -clock $input_clock -min -$skew_bfe [get_ports $input_ports] -clock_fall -add_delay;
+
+
+
+# Report Timing Template
+# report_timing -rise_from [get_ports $input_ports] -max_paths 20 -nworst 1 -delay_type min_max -name src_sync_edge_ddr_in_rise -file src_sync_edge_ddr_in_rise.txt;	  
+# report_timing -fall_from [get_ports $input_ports] -max_paths 20 -nworst 1 -delay_type min_max -name src_sync_edge_ddr_in_fall -file src_sync_edge_ddr_in_fall.txt; 
+          
+        
 
 set_property C_CLK_INPUT_FREQ_HZ 300000000 [get_debug_cores dbg_hub]
 set_property C_ENABLE_CLK_DIVIDER false [get_debug_cores dbg_hub]
--- source/rtl/ad9434_data.sv
+++ source/rtl/ad9434_data.sv
@@ -1,3 +1,4 @@
+
 `timescale 1ns/1ps
 module ad9434_data (
     input           clk_200m_in,
@@ -17,6 +18,7 @@ logic [5:0]     adc_data_q1;
 logic [5:0]     adc_data_q2;
 logic           adc_or;
 logic [5:0]     data_idelay;
+localparam      CLK_MODE = "BUR";
 
 /*                _ _ _         _ _ _
                  /     \       /     \
@@ -52,18 +54,6 @@ IBUFDS #(
 );
 
 
-
-BUFR #(
-    .BUFR_DIVIDE("BYPASS"),   // Values: "BYPASS, 1, 2, 3, 4, 5, 6, 7, 8" 
-    .SIM_DEVICE("7SERIES")  // Must be set to "7SERIES" 
-)
-BUFR_dco (
-    .O(dco_bufr),     // 1-bit output: Clock output port
-    .CE(),   // 1-bit input: Active high, clock enable (Divided modes only)
-    .CLR(), // 1-bit input: Active high, asynchronous clear (Divided modes only)
-    .I(dco)      // 1-bit input: Clock buffer input driven by an IBUF, MMCM or local interconnect
-);
-
 localparam CLKIN_PERIOD = 4.00;
 localparam CLKFBOUT_MULT_F = 4.00;
 localparam DIVCLK_DIVIDE = 1;
@@ -78,72 +68,136 @@ wire    clkin;
 wire    adc_clk;
 wire    dco_idelay;
 
-//assign clkin = dco_idelay;
-//assign clkin = dco;
-//assign adc_clk = clkoutbufg;
-assign adc_clk = dco_bufr;
+//
 
-
-
- // (* IODELAY_GROUP = idelay1 *) // Specifies group name for associated IDELAYs/ODELAYs and IDELAYCTRL
-
-
+if (CLK_MODE == "MMCM") begin :mmcm_mode
+    assign clkin = dco_idelay;
+    //assign adc_clk = clkoutbufg;
+   // BUFG bufg_clkout0 (.I(clkout0), .O(clkoutbufg)) ;
+   // BUFG bufg_fbout (.I(clkfbout), .O(clkfbin));
+    
+      clk_wiz_0 clk_wiz_inst
+     (
+      // Clock out ports
+      .adc_clk_90(adc_clk),     // output adc_clk_90
+      // Status and control signals
+      .reset(rst), // input reset
+      .locked(),       // output locked
+     // Clock in ports
+      .clk_in1(clkin));      // input clk_in1
 
 /*
-BUFG bufg_clkout0 (.I(clkout0), .O(clkoutbufg)) ;
-
-BUFG bufg_fbout (.I(clkfbout), .O(clkfbin));
-
-
-
-MMCME2_ADV #(
-    .BANDWIDTH		("OPTIMIZED"),  		
-    .CLKFBOUT_MULT_F	(CLKFBOUT_MULT_F),       			
-    .CLKFBOUT_PHASE		(0.0),     			
-    .CLKIN1_PERIOD		(CLKIN_PERIOD),  		
-    .CLKIN2_PERIOD		(CLKIN_PERIOD),  		
-    .CLKOUT0_DIVIDE_F	(CLKOUT0_DIVIDE_F),       			
-    .CLKOUT0_DUTY_CYCLE	(0.5), 				
-    .CLKOUT0_PHASE		(45.0),				
-    .CLKOUT0_USE_FINE_PS	("FALSE"),
-    .CLKOUT1_PHASE		(0),				
-    .CLKOUT1_DIVIDE		(CLKOUT1_DIVIDE),   				
-    .CLKOUT1_DUTY_CYCLE	(0.5), 				
-    .CLKOUT1_USE_FINE_PS	("FALSE"),    			
-    .COMPENSATION		("ZHOLD"),		
-    .DIVCLK_DIVIDE		(DIVCLK_DIVIDE),        		
-    .REF_JITTER1		(0.100))        		
-rx_mmcm_adv_inst (
-    .CLKFBOUT		(clkfbout),              		
-    .CLKFBOUTB		(),              		
-    .CLKFBSTOPPED		(),              		
-    .CLKINSTOPPED		(),              		
-    .CLKOUT0		(clkout0),      		
-    .CLKOUT0B		(),      			
-    .CLKOUT1		(),      		 
-    .PSCLK			(1'b0),  
-    .PSEN			(1'b0),  
-    .PSINCDEC		(1'b0),  
-    .PWRDWN			(1'b0), 
-    .LOCKED			(mmcm_locked),        		
-    .CLKFBIN		(clkfbin),			
-    .CLKIN1			(clkin),     	
-    .CLKIN2			(1'b0),		     		
-    .CLKINSEL		(1'b1),             		
-    .DADDR			(7'h00),            		
-    .DCLK			(1'b0),               		
-    .DEN			(1'b0),                		
-    .DI			    (16'h0000),        		
-    .DWE			(1'b0),                		
-    .RST			(rst)
-    );              	
+    MMCME2_ADV #(
+        .BANDWIDTH		("OPTIMIZED"),  		
+        .CLKFBOUT_MULT_F	(CLKFBOUT_MULT_F),       			
+        .CLKFBOUT_PHASE		(0.0),     			
+        .CLKIN1_PERIOD		(CLKIN_PERIOD),  		
+        .CLKIN2_PERIOD		(CLKIN_PERIOD),  		
+        .CLKOUT0_DIVIDE_F	(CLKOUT0_DIVIDE_F),       			
+        .CLKOUT0_DUTY_CYCLE	(0.5), 				
+        .CLKOUT0_PHASE		(90.0),				
+        .CLKOUT0_USE_FINE_PS	("FALSE"),
+        .CLKOUT1_PHASE		(0),				
+        .CLKOUT1_DIVIDE		(CLKOUT1_DIVIDE),   				
+        .CLKOUT1_DUTY_CYCLE	(0.5), 				
+        .CLKOUT1_USE_FINE_PS	("FALSE"),    			
+        .COMPENSATION		("ZHOLD"),		
+        .DIVCLK_DIVIDE		(DIVCLK_DIVIDE),        		
+        .REF_JITTER1		(0.100))        		
+    mmcm_adv_inst (
+        .CLKFBOUT		(clkfbout),              		
+        .CLKFBOUTB		(),              		
+        .CLKFBSTOPPED		(),              		
+        .CLKINSTOPPED		(),              		
+        .CLKOUT0		(clkout0),      		
+        .CLKOUT0B		(),      			
+        .CLKOUT1		(),      		 
+        .PSCLK			(1'b0),  
+        .PSEN			(1'b0),  
+        .PSINCDEC		(1'b0),  
+        .PWRDWN			(1'b0), 
+        .LOCKED			(mmcm_locked),        		
+        .CLKFBIN		(clkfbin),			
+        .CLKIN1			(clkin),     	
+        .CLKIN2			(1'b0),		     		
+        .CLKINSEL		(1'b1),             		
+        .DADDR			(7'h00),            		
+        .DCLK			(1'b0),               		
+        .DEN			(1'b0),                		
+        .DI			    (16'h0000),        		
+        .DWE			(1'b0),                		
+        .RST			(rst)
+        );              	
 */
+end
+
+else begin:idelay_mode
+    
+    BUFR #(
+    .BUFR_DIVIDE("BYPASS"),   // Values: "BYPASS, 1, 2, 3, 4, 5, 6, 7, 8" 
+    .SIM_DEVICE("7SERIES")  // Must be set to "7SERIES" 
+    )
+    BUFR_dco (
+        .O(dco_bufr),     // 1-bit output: Clock output port
+        .CE(),   // 1-bit input: Active high, clock enable (Divided modes only)
+        .CLR(), // 1-bit input: Active high, asynchronous clear (Divided modes only)
+        .I(dco)      // 1-bit input: Clock buffer input driven by an IBUF, MMCM or local interconnect
+    );
+
+/*
+    IDELAYE2 #(
+        .CINVCTRL_SEL("FALSE"),          // Enable dynamic clock inversion (FALSE, TRUE)
+        .DELAY_SRC("IDATAIN"),           // Delay input (IDATAIN, DATAIN)
+        .HIGH_PERFORMANCE_MODE("TRUE"), // Reduced jitter ("TRUE"), Reduced power ("FALSE")
+        .IDELAY_TYPE("FIXED"),           // FIXED, VARIABLE, VAR_LOAD, VAR_LOAD_PIPE
+        .IDELAY_VALUE(1),                // Input delay tap setting (0-31)
+        .PIPE_SEL("FALSE"),              // Select pipelined mode, FALSE, TRUE
+        .REFCLK_FREQUENCY(200.0),        // IDELAYCTRL clock input frequency in MHz (190.0-210.0, 290.0-310.0).
+        .SIGNAL_PATTERN("CLOCK")          // DATA, CLOCK input signal
+    )
+    IDELAYE2_clk (
+        .CNTVALUEOUT(), // 5-bit output: Counter value output
+        .DATAOUT(dco_idelay),         // 1-bit output: Delayed data output
+        .C(clk_200m_in),                     // 1-bit input: Clock input
+        .CE(1'b0),                   // 1-bit input: Active high enable increment/decrement input
+        .CINVCTRL(1'b0),       // 1-bit input: Dynamic clock inversion input
+        .CNTVALUEIN(),   // 5-bit input: Counter value input
+        .DATAIN(),           // 1-bit input: Internal delay data input
+        .IDATAIN(dco),         // 1-bit input: Data input from the I/O
+        .INC(1'b0),                 // 1-bit input: Increment / Decrement tap delay input
+        .LD(1'b0),                   // 1-bit input: Load IDELAY_VALUE input
+        .LDPIPEEN(1'b0),       // 1-bit input: Enable PIPELINE register to load data input
+        .REGRST(rst)            // 1-bit input: Active-high reset tap-delay input
+    );
+    */
+
+    assign adc_clk = dco_bufr;
+end
+
 IDELAYCTRL IDELAYCTRL_dco (
     .RDY(),       // 1-bit output: Ready output
     .REFCLK(clk_200m_in), // 1-bit input: Reference clock input
     .RST(rst)        // 1-bit input: Active high reset input
  );
+ 
+
+
 genvar i;
+generate
+    for (i = 0; i < 6; i = i + 1) begin: adc_data_bufds
+        IBUFDS #(
+            .DIFF_TERM("TRUE"),       // Differential Termination
+            .IBUF_LOW_PWR("TRUE"),     // Low power="TRUE", Highest performance="FALSE" 
+            .IOSTANDARD("LVDS")     // Specify the input I/O standard
+        ) IBUFDS_adc (
+            .O(data_bufds[i]),  // Buffer output
+            .I(adc0_din_p[i]),  // Diff_p buffer input (connect directly to top-level port)
+            .IB(adc0_din_n[i]) // Diff_n buffer input (connect directly to top-level port)
+        );
+    end
+endgenerate
+
+
 
 generate 
     for (i = 0; i < 6; i = i + 1) begin: idelay
@@ -154,12 +208,12 @@ generate
         .DELAY_SRC("IDATAIN"),           // Delay input (IDATAIN, DATAIN)
         .HIGH_PERFORMANCE_MODE("TRUE"), // Reduced jitter ("TRUE"), Reduced power ("FALSE")
         .IDELAY_TYPE("FIXED"),           // FIXED, VARIABLE, VAR_LOAD, VAR_LOAD_PIPE
-        .IDELAY_VALUE(19),                // Input delay tap setting (0-31)
+        .IDELAY_VALUE(1),                // Input delay tap setting (0-31)
         .PIPE_SEL("FALSE"),              // Select pipelined mode, FALSE, TRUE
         .REFCLK_FREQUENCY(200.0),        // IDELAYCTRL clock input frequency in MHz (190.0-210.0, 290.0-310.0).
         .SIGNAL_PATTERN("DATA")          // DATA, CLOCK input signal
     )
-    IDELAYE2_dco (
+    IDELAYE2_data (
         .CNTVALUEOUT(), // 5-bit output: Counter value output
         .DATAOUT(data_idelay[i]),         // 1-bit output: Delayed data output
         .C(clk_200m_in),                     // 1-bit input: Clock input
@@ -175,20 +229,8 @@ generate
     );
     end
 endgenerate
-generate
-    for (i = 0; i < 6; i = i + 1) begin: adc_data_bufds
-        IBUFDS #(
-            .DIFF_TERM("TRUE"),       // Differential Termination
-            .IBUF_LOW_PWR("TRUE"),     // Low power="TRUE", Highest performance="FALSE" 
-            .IOSTANDARD("LVDS")     // Specify the input I/O standard
-        ) IBUFDS_adc (
-            .O(data_bufds[i]),  // Buffer output
-            .I(adc0_din_p[i]),  // Diff_p buffer input (connect directly to top-level port)
-            .IB(adc0_din_n[i]) // Diff_n buffer input (connect directly to top-level port)
-        );
-    end
-endgenerate
 
+//assign data_idelay = data_bufds;
 
 generate
 
